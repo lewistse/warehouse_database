b5f91b54dae0e412adb5339f68065442e3f7e9be
ben <ben.chong@airpoint.com.hk>
2017-11-17 16:56:18 +0800

    combined part of warehouse and quickstart-android/database apps but not able to retrieve data from firebase yet

---
+++ database/app/src/main/java/com/google/firebase/quickstart/database/NewOrderActivity.java
@@ -0,0 +1,172 @@
+package com.google.firebase.quickstart.database;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.quickstart.database.models.Post;
+import com.google.firebase.quickstart.database.models.User;
+
+import com.google.firebase.quickstart.database.helper.PhotoHelper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NewOrderActivity extends BaseActivity {
+
+    private static final String TAG = "NewOrderActivity";
+    private static final String REQUIRED = "Required";
+
+    // [START declare_database_ref]
+    private DatabaseReference mDatabase;
+    // [END declare_database_ref]
+
+    private EditText mTitleField;
+    private EditText mNewOrderField;
+    private EditText mBodyField;
+    private FloatingActionButton mSubmitButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_new_post);
+        setContentView(R.layout.activity_new_order);
+
+        // [START initialize_database_ref]
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        // [END initialize_database_ref]
+
+        //mTitleField = findViewById(R.id.field_title);
+        mNewOrderField = findViewById(R.id.new_order_title);
+        //mBodyField = findViewById(R.id.field_body);
+        mSubmitButton = findViewById(R.id.fab_new_order);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitPost();
+            }
+        });
+    }
+
+    private void submitPost() {
+        //final String title = mTitleField.getText().toString();
+        final String orderNumber = mNewOrderField.getText().toString();
+        //final String body = mBodyField.getText().toString();
+
+        final String driverName = "driver Name";
+        final String driverHkid = "HKID number";
+        final String carPlateNumber = "Car plate number";
+        final Boolean fulfilled = false;
+
+
+        // Title is required
+        if (TextUtils.isEmpty(orderNumber)) {
+            //mTitleField.setError(REQUIRED);
+            mNewOrderField.setError(REQUIRED);
+            return;
+        }
+
+        /*// Body is required
+        if (TextUtils.isEmpty(body)) {
+            mBodyField.setError(REQUIRED);
+            return;
+        }*/
+
+        // Disable button so there are no multi-posts
+        setEditingEnabled(false);
+        Toast.makeText(this, "Synchronizing...", Toast.LENGTH_SHORT).show();
+
+        // [START single_value_read]
+        final String userId = getUid();
+        mDatabase.child("users").child(userId).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user value
+                        User user = dataSnapshot.getValue(User.class);
+
+                        // [START_EXCLUDE]
+                        if (user == null) {
+                            // User is null, error out
+                            Log.e(TAG, "User " + userId + " is unexpectedly null");
+                            Toast.makeText(NewOrderActivity.this,
+                                    "Error: could not fetch user.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Write new post
+                            //writeNewPost(userId, user.username, title, body);
+                            writeNewPost(userId, user.username, orderNumber, driverName, driverHkid, carPlateNumber);
+                        }
+
+                        // Finish this Activity, back to the stream
+                        setEditingEnabled(true);
+                        finish();
+                        // [END_EXCLUDE]
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                        // [START_EXCLUDE]
+                        setEditingEnabled(true);
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END single_value_read]
+    }
+
+    private void setEditingEnabled(boolean enabled) {
+        //mTitleField.setEnabled(enabled);
+        mNewOrderField.setEnabled(enabled);
+        //mBodyField.setEnabled(enabled);
+        if (enabled) {
+            mSubmitButton.setVisibility(View.VISIBLE);
+        } else {
+            mSubmitButton.setVisibility(View.GONE);
+        }
+    }
+
+    // [START write_fan_out]
+    //private void writeNewPost(String userId, String username, String title, String body) {
+    private void writeNewPost(String userId, String username, String orderNumber, String driverName, String driverHkid, String carPlateNumber) {
+        // Create new post at /user-posts/$userid/$postid and at
+        // /posts/$postid simultaneously
+        String key = mDatabase.child("orders").push().getKey();
+        //Post post = new Post(userId, username, title, body);
+
+
+        Post order = new Post(userId, username, orderNumber, driverName, driverHkid, carPlateNumber);   //updated ../models/Post.java
+        Map<String, Object> postValues = order.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/orders/" + key, postValues);
+        //childUpdates.put("/posts/" + key, postValues);
+        //childUpdates.put("/user-posts/" + userId + "/" + key, postValues);
+
+        mDatabase.updateChildren(childUpdates);
+    }
+    // [END write_fan_out]
+
+
+}
